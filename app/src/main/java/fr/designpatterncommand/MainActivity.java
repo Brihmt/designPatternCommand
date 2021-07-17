9fb6603f881d9e1f979ae12ca593b9d4fa83bc2a
Brihmt <ivanleteuff@outlook.fr>
2021-07-17 16:26:26 +0200

    initialisation du projet

---
+++ app/src/main/java/fr/designpatterncommand/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.designpatterncommand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
