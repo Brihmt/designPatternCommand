650ba8da419680b7a6f9466998208827d7421c99
mattheo <mattheo.lecrivain@etu.univ-nantes.fr>
2021-07-20 16:08:17 +0200

    Character class added

--- app/src/main/java/fr/designpatterncommand/model/Character.java
+++ app/src/main/java/fr/designpatterncommand/model/Character.java
@@ -1,4 +1,48 @@
 package fr.designpatterncommand.model;
+
+import android.widget.ImageView;
+
+import java.util.Vector;
+
 // TODO: Ajouter une image du perso/ ses déplacements
 public class Character {
+    private int posX, posY;
+    private int characterImg;
+
+    public Character(){
+        posX = 1;
+        posY = 1;
+        //characterImg = R.drawable.character;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void moveTop(){
+        if(posY>1) posY--;
+    }
+
+    public void moveBottom(){
+        if(posY<5) posY++;
+    }
+
+    public void moveLeft(){
+        if(posX>1) posX--;
+    }
+
+    public void moveRight(){
+        if(posX<8) posX++;
+    }
+
+    public void move(Movement movement){
+        if(movement==Movement.TOP) moveTop();
+        if(movement==Movement.BOTTOM) moveBottom();
+        if(movement==Movement.LEFT) moveLeft();
+        if(movement==Movement.RIGHT) moveRight();
+    }
 }
--- app/src/main/java/fr/designpatterncommand/model/ManagerAction.java
+++ app/src/main/java/fr/designpatterncommand/model/ManagerAction.java
@@ -22,6 +22,6 @@ public class ManagerAction {
     }
 
     public void execCommandList(){
-
+        commandList.forEach(command -> character.move(command.operation()));
     }
 }
